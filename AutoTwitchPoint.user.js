8122eb969a20e5a45cd6dfb502646fbd62a2436b
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2021-05-30 20:45:02 +0800

    Update AutoTwitchPoint.user.js

--- AutoTwitchPoint.user.js
+++ AutoTwitchPoint.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name        Auto Claim Twitch Channel Point
-// @namespace   https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point
-// @Version     0.2
-// @description Automatically claims the randomly appearing bonus chest on any open twitch stream.
-// @author      adeFuLoDgu
-// @include     *://*.twitch.tv/*
-// @updateURL   https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point/raw/main/AutoTwitchPoint.user.js
-// @downloadURL https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point/raw/main/AutoTwitchPoint.user.js
-// @run-at      document-end
-// @grant       none
+// @name         Auto Claim Twitch Channel Point
+// @namespace    https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point
+// @Version      0.2
+// @description  Automatically claims the randomly appearing bonus chest on any open twitch stream.
+// @author       adeFuLoDgu
+// @include      *://*.twitch.tv/*
+// @updateURL    https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point/raw/main/AutoTwitchPoint.user.js
+// @downloadURL  https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point/raw/main/AutoTwitchPoint.user.js
+// @run-at       document-end
+// @grant        none
 // ==/UserScript==
 
 (function() {
