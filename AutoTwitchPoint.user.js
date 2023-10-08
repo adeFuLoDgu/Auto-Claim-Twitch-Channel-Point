370eff103245dcd81ffa0ac09beef4938e84b5fc
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2023-10-08 14:31:07 +0800

    Update AutoTwitchPoint.user.js

--- AutoTwitchPoint.user.js
+++ AutoTwitchPoint.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Auto Claim Twitch Channel Point
 // @namespace   https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point
-// @version     0.3
+// @version     0.4
 // @description Automatically claims the randomly appearing bonus chest on any open twitch stream.
 // @author      adeFuLoDgu
 // @match     	*://*.twitch.tv/*
@@ -11,30 +11,31 @@
 // @grant       none
 // ==/UserScript==
 
-function waitForElm(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
-
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                observer.disconnect();
-                resolve(document.querySelector(selector));
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
-}
-
 (function() {
-  waitForElm('.claimable-bonus__icon').then(function(elm) {
-    if (elm !== null) {
-      elm[0].click();
+    'use strict';
+    function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
     }
-  });
+
+    waitForElm('.claimable-bonus__icon').then(function(elm) {
+        if (elm !== null) {
+            elm.click();
+        }
+    });
 })();
